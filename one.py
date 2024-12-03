9c3967ec1da32424061879490a84b1c2ec7541e6
aMetallurgist <joshua.d1434@gmail.com>
2024-12-02 22:43:43 -0800

    Day 2 first challenge

--- one.py
+++ one.py
@@ -1,4 +1,8 @@
 import heapq
+from dataclasses import dataclass
+
+l1 = sorted([3, 4, 2, 1, 3, 3])
+l2 = sorted([4, 3, 5, 3, 9, 3])
 
 def list_distance(filepath: str):
     first_ids, second_ids = list(), list()
@@ -20,6 +24,30 @@ def list_distance(filepath: str):
 
     return result
 
+def similarity_score(filepath: str):
+    '''
+    Calculate the similarity score of two lists, where each 
+    element's similarity is measured as (value * frequency_in_other_list)
+    '''
+    first_ids, second_freq = list(), dict()
+
+    with open(filepath) as file:
+        for line in file:
+            first, second = line.split()
+            first, second = int(first), int(second)
+            first_ids.append(first)
+            frequency = second_freq.get(second, 0)
+            second_freq[second] = frequency + 1
+    
+    result = 0
+    for location_id in first_ids:
+        freq = second_freq.get(location_id, 0)
+        result += location_id * freq
+
+    return result
+
 if __name__ == '__main__':
     # 3569916
     print(list_distance("one.txt"))
+    # 26407426
+    print(similarity_score("one.txt"))
