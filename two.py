a52615debc6ead503b1b96d883d34188041d1d8c
aMetallurgist <joshua.d1434@gmail.com>
2024-12-03 00:34:46 -0800

    Update Day 2 second challenge to avoid repeated copies of the list, by using generated clean lists instead

--- two.py
+++ two.py
@@ -1,44 +1,51 @@
-# one report per line
-# each report is list of levels
-# a report is safe if levels are 1) all increasing or all decreasing
-# adn 2) adjacent levels differ by at least one and at most three
+from typing import Optional, Iterator, List
 
-from typing import List, Optional
-def unsafe_report_level(report: List[int]) -> Optional[int]:
-    if len(report) <= 1:
-        return True, None
-
+def unsafe_report_level(report: Iterator[int]) -> Optional[int]:
+    try:
+        cur = next(report)
+    except StopIteration:
+        return None
+    
     overall_direction = None
-    for idx in range(len(report) - 1):
-        cur, next = report[idx], report[idx + 1]
-        diff = cur - next
+    idx = 0
+    for el in report:
+        next_el = el
+        diff = cur - next_el
         abs_diff = abs(diff)
         if abs_diff < 1 or abs_diff > 3:
             return idx
-        
         cur_direction = diff // abs_diff
         if overall_direction is None:
             overall_direction = cur_direction
         elif overall_direction != cur_direction:
             return idx
         
+        cur = next_el
+        idx += 1        
     return None
+
+def generate_clean_report(report: List[int], start_idx: int, idx_to_remove: int):
+    for count, el in enumerate(report, start=start_idx):
+        idx = count - start_idx
+        if idx != idx_to_remove:
+            yield el
 
 def safe_report_count(filepath: str, enable_dampener: bool) -> int:
     result = 0
     with open(filepath) as file:
         for line in file:
             report = [int(el) for el in line.split()]
-            unsafe_idx = unsafe_report_level(report)
+            unsafe_idx = unsafe_report_level((iter(report)))
             if unsafe_idx is None:
                 result += 1
             elif enable_dampener:
+                # Restart unsafe level search starting at last known safe pair of 
+                # levels. Generate three potential cleaned reports - by removing the
+                # level where the safety issue was found and its neighbors.
                 last_safe_idx = max(unsafe_idx - 2, 0)
                 for remove_idx in (unsafe_idx - 1, unsafe_idx, unsafe_idx + 1):
-                    if (remove_idx < 0) or (remove_idx >= len(report)):
-                        continue
-                    cleaned_report = report[last_safe_idx:remove_idx] + report[remove_idx + 1:] 
-                    if unsafe_report_level(cleaned_report) is None:
+                    clean_report_generator = generate_clean_report(report, last_safe_idx, remove_idx)
+                    if unsafe_report_level(clean_report_generator) is None:
                         result += 1
                         break
     return result
