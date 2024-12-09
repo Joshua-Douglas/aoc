21e83241d43d83bb1dcd3223246b516d0b476a0e
aMetallurgist <joshua.d1434@gmail.com>
2024-12-08 16:51:40 -0800

    Add day six, second challenge, using brute force.

--- test/test_challenges.py
+++ test/test_challenges.py
@@ -1,6 +1,9 @@
+import pytest
+
 from twenty_twentyfour.four import count_word_in_grid, count_x_mas
 from twenty_twentyfour.five import valid_updates, invalid_updates
-from twenty_twentyfour.six import count_guard_positions
+from twenty_twentyfour.six import count_guard_positions, guard_is_in_loop, count_guard_loops
+from twenty_twentyfour.datatypes import CharGrid, Direction
 
 def test_four_first_example():
     example = ('MMMSXXMASM',
@@ -56,21 +59,39 @@ def test_five_second(data_dir):
 
     assert invalid_updates(updates, restricted_pages) == 4130
 
-def test_six_example():
-    example = ('....#.....',
-        '.........#',
-        '..........',
-        '..#.......',
-        '.......#..',
-        '..........',
-        '.#..^.....',
-        '........#.',
-        '#.........',
-        '......#...',
-    )
-    assert count_guard_positions(example) == 41
+def test_six_example(data_dir):
+    with open(data_dir / 'six_example.txt') as f:
+        source = f.readlines()
+    assert count_guard_positions(source) == 41
 
 def test_six_first(data_dir):
     with open(data_dir / 'six.txt') as f:
         source = f.readlines()
     assert count_guard_positions(source) == 4722
+
+def test_not_in_loop(data_dir):
+    with open(data_dir / 'six_example.txt') as f:
+        source = f.readlines()
+    start_pos = (4, 6)
+    grid = CharGrid(source)
+    assert guard_is_in_loop(grid, start_pos, Direction.NORTH) == False
+
+@pytest.mark.parametrize("input_file", [
+    'six_loop1.txt',
+    'six_loop2.txt',
+    'six_loop3.txt',
+    'six_loop4.txt',
+    'six_loop5.txt',
+    'six_loop6.txt',
+])
+def test_in_loop(input_file, data_dir):
+    with open(data_dir / input_file) as f:
+        source = f.readlines()
+    start_pos = (4, 6)
+    grid = CharGrid(source)
+    assert guard_is_in_loop(grid, start_pos, Direction.NORTH)
+
+def test_six_second(data_dir):
+    with open(data_dir / 'six.txt') as f:
+        source = f.readlines()
+    assert count_guard_loops(source) == 1602
